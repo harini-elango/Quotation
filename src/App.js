2f603741abbca079046c9fd06ddf04802b6085d1
harini-elango <harini17elango@gmail.com>
2024-07-31 09:35:55 +0530

    Initial Frontend Commit for Quotation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import CardComponent from './components/card.js'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Card Component <code>src/components/card.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +22,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <CardComponent />
+      </main>
     </div>
   );
 }
